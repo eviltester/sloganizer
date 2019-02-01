9b8c9b04873ae0e277aab91ebcc64ecabf01e7d0
alan <alan@compendiumdev.co.uk>
2019-02-01 00:05:29 +0000

    split out the json into a separate file to support reuse on other platforms and added a php prototype to use the json data

--- html/js/sloganizer/sloganizer.js
+++ html/js/sloganizer/sloganizer.js
@@ -240,6 +240,36 @@ var sentences = [
     // OK! To be honest, I do it 'cause it's fun.
 ]
 
+// to create json file for other apps
+var stringify = {phrases, sentences};
+
+// to read in the json file
+/*
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'js/sloganizer/sloganizer_data.js', false);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+function init() {
+    loadJSON(function(response) {
+        // Parse JSON string into object
+        var actual_JSON = JSON.parse(response);
+        phrases = actual_JSON.phrases;
+        sentences = actual_JSON.sentences;
+    });
+}
+*/
+
+
 function randomNumberUpTo(theVal){
     return Math.floor(Math.random() * theVal)
 }
